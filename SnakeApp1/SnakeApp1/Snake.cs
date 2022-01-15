d01eee3ab7be116b5fc14129c6e82e9e41952b91
OlaChu <olenka-chuvakov@mail.ru>
2022-01-16 02:48:42 +0300

    Добыть еду, уйти от препятствий

--- SnakeApp1/SnakeApp1/Snake.cs
+++ SnakeApp1/SnakeApp1/Snake.cs
@@ -40,7 +40,16 @@ namespace SnakeApp1
             nextPoint.Move(1, direction);
             return nextPoint;
         }
-
+         internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
         public void HandleKey(ConsoleKey key)
         {
             if (key == ConsoleKey.LeftArrow)
